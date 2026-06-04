5bd652d5c71596d2e5a77f148b250f740301bc26
Gabriel123G <gabi132002@gmail.com>
2026-06-04 08:19:52 -0500

    ajustes en el boton eliminar cuando es nulo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -147,7 +147,7 @@ class AdminController extends Controller
         $imageService = new ImagesServices();
         $imageService->setAccessToken($token);
 
-        if ($proyecto->images_urls){
+        if (!is_null($proyecto->images_urls)){
         foreach ($proyecto->images_urls as $image) {
             $imageService->deleteFile($image->url);
             $image->delete();
